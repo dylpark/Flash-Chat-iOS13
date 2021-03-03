4d89ead2f719a72015f09f7756e2cf11d0a5b46d
Dylan Park <dylanpark.design@gmail.com>
2021-03-03 14:59:19 +1000

    Constants

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -23,7 +23,7 @@ class RegisterViewController: UIViewController {
                     print (e.localizedDescription)
                 } else {
                     //Navigate to ChatViewController
-                    self.performSegue(withIdentifier: Constants.registerSegue, sender: self)
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
                 }
             }
         }

eb7804912d13d23902ba7a8cb6bdc42df5b5409c
Dylan Park <dylanpark.design@gmail.com>
2021-03-09 16:58:49 +1000

    Update ChatViewController.swift

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -98,14 +98,14 @@ extension ChatViewController: UITableViewDataSource {
         let message = messages[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label.text = messages.body
+        cell.label.text = message.body
         
         //Message from current user
         if message.sender == Auth.auth().currentUser?.email {
             cell.leftImageView.isHidden = true
             cell.rightImageView.isHidden = false
             cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
-            cell.label.textColor = UIColor(neamed: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
         }
         
         //Message from another sender
@@ -113,7 +113,7 @@ extension ChatViewController: UITableViewDataSource {
             cell.leftImageView.isHidden = false
             cell.rightImageView.isHidden = true
             cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
-            cell.label.textColor = UIColor(neamed: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
         }
         
         return cell

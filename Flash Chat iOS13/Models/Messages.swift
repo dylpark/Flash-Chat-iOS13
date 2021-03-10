c3be8c17d5b6fb6e92ea7acb1fb282c495475bf3
Dylan Park <dylanpark.design@gmail.com>
2021-03-10 10:54:34 +1000

    Create Messages.swift

---
+++ Flash Chat iOS13/Models/Messages.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Dylan Park on 3/3/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //eMail Address
+    let body: String
+}
